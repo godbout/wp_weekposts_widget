b0c89b5dc8d10c464f5a859b1ecc171f77db917f
Guill Lo <guill.bout@gmail.com>
2014-03-19 10:18:54 +0800

    Replace an old WP function by a new one

--- weekposts-widget.php
+++ weekposts-widget.php
@@ -26,7 +26,7 @@ class WeekpostWidget extends WP_Widget
 ?>
     <p>
         <label for="<?php echo $this->get_field_id('title'); ?>"></label>
-        Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" />
+        Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
     </p>
     <p>
         <label for="<?php echo $this->get_field_id('type'); ?>"></label>
